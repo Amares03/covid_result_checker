0561293863e25cc2fa7708f0045f2844e0bf0214
Amares03 <amaressabit0314@gmail.com>
2022-05-10 10:04:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:covid_result_checker/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    ),
+  );
+}
